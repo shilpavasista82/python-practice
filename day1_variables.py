cd6eb4de047f9f0f0d7be872b73ed234d197f93f
Shilpa <shilpavasista82@gmail.com>
2026-08-29 19:49:09 +0530

    day 1 variables and datatypes

---
+++ day1_variables.py
@@ -0,0 +1,18 @@
+city="Mumbai"
+print(city)
+
+age=25
+temperature = -5
+height = 5.9
+price = 99.99
+name = 'Alex'
+value ="25"
+is_raining = True
+
+
+print(type(city))
+print(5+3)
+print("alex" + "kumar")
+
+population=200000000
+print(f"{city} has a population of {population}")
